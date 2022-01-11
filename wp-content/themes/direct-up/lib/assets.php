3d2d3d9bc570db73094a3ee8b0d433f23824a14f
MoemenGaber <moemen.gaber.2@gmail.com>
2022-01-11 08:17:44 +0200

    [DU-2] Added version for assets

--- wp-content/themes/direct-up/lib/assets.php
+++ wp-content/themes/direct-up/lib/assets.php
@@ -12,16 +12,18 @@ class require_assets
 
     public function require_styles()
     {
-        wp_enqueue_style('plugins', get_template_directory_uri() . '/assets/css/inc/plugins.css', array(), '2.0.0', 'all');
-        wp_enqueue_style('directup-style', get_template_directory_uri() . '/assets/css/style.css', array(), '2.0.0', 'all');
+        $version=date('Ymdhs');
+        wp_enqueue_style('plugins', get_template_directory_uri() . '/assets/css/inc/plugins.css', array(), $version, 'all');
+        wp_enqueue_style('directup-style', get_template_directory_uri() . '/assets/css/style.css', array(),$version, 'all');
 
     }
 
     public function require_scripts()
     {
-        wp_enqueue_script('jquery-plugins', get_template_directory_uri() . '/assets/js/jquery-plugins.js', array(), '2.0.0', 'true' );
-        wp_enqueue_script('directup-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), '2.0.0', 'true' );
-        wp_enqueue_script('custom-scripts', get_template_directory_uri() . '/assets/js/custom-scripts.js', array(), '2.0.0', 'true' );
+        $version=date('Ymdhs');
+        wp_enqueue_script('jquery-plugins', get_template_directory_uri() . '/assets/js/jquery-plugins.js', array(),$version, 'true' );
+        wp_enqueue_script('directup-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(),$version, 'true' );
+        wp_enqueue_script('custom-scripts', get_template_directory_uri() . '/assets/js/custom-scripts.js', array(),$version, 'true' );
 
         wp_localize_script( 'custom-scripts', 'my_ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 
